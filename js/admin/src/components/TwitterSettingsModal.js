0a2152a7295c3d90b0181fc4bb915610cfdc906c
Toby Zerner <toby.zerner@gmail.com>
2015-09-18 13:30:08 +0930

    Use SettingsModal component

--- js/admin/src/components/TwitterSettingsModal.js
+++ js/admin/src/components/TwitterSettingsModal.js
@@ -1,64 +1,25 @@
-import Modal from 'flarum/components/Modal';
-import Button from 'flarum/components/Button';
-import saveConfig from 'flarum/utils/saveConfig';
+import SettingsModal from 'flarum/components/SettingsModal';
 
-export default class TwitterSettingsModal extends Modal {
-  constructor(...args) {
-    super(...args);
-
-    this.apiKey = m.prop(app.config['twitter.api_key'] || '');
-    this.apiSecret = m.prop(app.config['twitter.api_secret'] || '');
-  }
-
+export default class TwitterSettingsModal extends SettingsModal {
   className() {
     return 'TwitterSettingsModal Modal--small';
   }
 
   title() {
-    return 'TwiTter Settings';
+    return 'Twitter Settings';
   }
 
-  content() {
-    return (
-      <div className="Modal-body">
-        <div className="Form">
-          <div className="Form-group">
-            <label>API Key</label>
-            <input className="FormControl" value={this.apiKey()} oninput={m.withAttr('value', this.apiKey)}/>
-          </div>
+  form() {
+    return [
+      <div className="Form-group">
+        <label>API Key</label>
+        <input className="FormControl" bidi={this.setting('twitter.api_key')}/>
+      </div>,
 
-          <div className="Form-group">
-            <label>API Secret</label>
-            <input className="FormControl" value={this.apiSecret()} oninput={m.withAttr('value', this.apiSecret)}/>
-          </div>
-
-          <div className="Form-group">
-            <Button
-              type="submit"
-              className="Button Button--primary TwitterSettingsModal-save"
-              loading={this.loading}>
-              Save Changes
-            </Button>
-          </div>
-        </div>
+      <div className="Form-group">
+        <label>API Secret</label>
+        <input className="FormControl" bidi={this.setting('twitter.api_secret')}/>
       </div>
-    );
-  }
-
-  onsubmit(e) {
-    e.preventDefault();
-
-    this.loading = true;
-
-    saveConfig({
-      'twitter.api_key': this.apiKey(),
-      'twitter.api_secret': this.apiSecret()
-    }).then(
-      () => this.hide(),
-      () => {
-        this.loading = false;
-        m.redraw();
-      }
-    );
+    ];
   }
 }
